53c4c096f8e8133d5181b6488d80726183d8c7d8
Cesar Huamani <cesarhuamanicastro@gmail.com>
2024-10-26 03:01:59 -0500

    Initial Web

---
+++ src/Dcode.CisorFE/Data/Migrations/20241026074112_Initial account.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dcode.CisorFE.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialaccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
